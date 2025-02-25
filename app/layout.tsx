f037ff85aa8c2fdd98208b9ac9416026211417dd
Dhaneshen1810 <dan.moonian@gmail.com>
2025-02-25 15:33:19 -0700

    chore: updated layout background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="font-sans">
+      <body className="font-sans bg-lightPink">
         {children}
         <Toaster />
       </body>
