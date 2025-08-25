5d924c9b478f43e6d4587bc5e6649356d53ac826
mothercoconuts93 <guledtheboss21@gmail.com>
2025-08-25 11:41:05 -0600

    updated, with addition to calendar page that redirects you to view all task list

--- components/boxes/footer-box/footer.tsx
+++ components/boxes/footer-box/footer.tsx
@@ -9,7 +9,9 @@ const Footer = () => {
         <div className="flex items-center justify-center gap-6">
 
           <button
-            className="inline-flex h-12 w-12 items-center justify-center rounded-xl border border-neutral-200 bg-white shadow-sm hover:bg-neutral-50 active:scale-95 transition hidden"
+            className="inline-flex h-12 w-12 items-center justify-center rounded-xl 
+                       border border-neutral-200 bg-white shadow-sm hover:bg-neutral-50 
+                       active:scale-95 transition hidden"
             aria-label="Settings"
           >
             <Settings className="h-6 w-6" stroke="gray" strokeWidth={2.5} />
@@ -17,13 +19,12 @@ const Footer = () => {
 
           <AddTaskModal />
 
-          {/**
-           * clicking the calendar icon will naviate you to task list page.
-           */}
           <Link
             href="/calendar"
             aria-label="Open calendar"
-            className="inline-flex h-12 w-12 items-center justify-center rounded-xl border border-neutral-200 bg-white shadow-sm hover:bg-neutral-50 active:scale-95 transition"
+            className="inline-flex h-12 w-12 items-center justify-center 
+                       rounded-xl border border-neutral-200 bg-white shadow-sm
+                       hover:bg-neutral-50 active:scale-95 transition"
           >
             <CalendarRange className="h-6 w-6" stroke="gray" strokeWidth={2.5} />
           </Link>
